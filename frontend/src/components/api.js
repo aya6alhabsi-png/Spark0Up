c1d4cf90a39489f1a68c7aaec2df6eed11084928
Aya_Abdullah <aya6alhabsi@gmail.com>
2026-05-18 16:07:37 +0400

    Update api.js

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const API_URL = "http://localhost:5000";
-
+export const API_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:5000";
 export function getToken() {
   return localStorage.getItem("token") || localStorage.getItem("authToken") || localStorage.getItem("jwt") || "";
 }
